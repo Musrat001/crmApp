15eb99c1b03eaf20a74db2923fc700083637f864
Musrat001 <mohdmusrat73@gmail.com>
2026-06-01 02:38:03 +0530

    added the middleware for body validation

--- middlewares/verifyUserBody.middlewares.js
+++ middlewares/verifyUserBody.middlewares.js
@@ -1,14 +1,11 @@
 const User = require("../models/user.models.js");
 const constant = require("../utils/constant.js");
-validateUserRequestBody = async (req, res, next) => {
 
-    // console.log(`request data is ${req.body.user}`);
-    // console.log(`request data is ${req.body.userId}`);
-    // console.log(`request data is ${req.body.email}`);
-    // console.log(`request data is ${req.body.password}`);
-    // console.log(`request data is ${req.body.userType}`);
+
+
+validateUserRequestBody = async (req, res, next) => {
     console.log(req.method);
-    
+
     if (!req.body.name) {
         res.status(400).send({
             message: "User name required.."
@@ -63,6 +60,34 @@ validateUserRequestBody = async (req, res, next) => {
 
 }
 
+
+const validateUserStatusAndUserType = (req, res, next) => {
+    // validating the userType 
+
+    const userType = req.body.userType;
+    const validUserTypes = [constant.userTypes.admin, constant.userTypes.customer, constant.userTypes.engineer];
+    if (userType && !validUserTypes.includes(userType)) {
+        res.status(402).send({
+            message: "Provide valid User Type, Valide user types are CUSTOMER | ENGINEER | ADMIN"
+        });
+    }
+
+    // validating the user Status
+
+    const userStatus = req.body.userStatus;
+    const validUserStatus = [constant.userStatus.approved, constant.userStatus.blocked, constant.userStatus.pending];
+
+
+    if (userStatus && !validUserStatus.includes(userStatus)) {
+        res.status(402).send({
+            message: "provide proper user Status, vlaid user status are APPROVED | PENDING | BLOCKED"
+        });
+    }
+
+    next();
+}
+
 module.exports = {
-    validateUserRequestBody: validateUserRequestBody
+    validateUserRequestBody: validateUserRequestBody,
+    validateUserStatusAndUserType: validateUserStatusAndUserType
 }
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const userController = require("../controllers/user.controllers");
 const router = express.Router();
 const authMW = require("../middlewares/authjwt.middlewares.js");
+const verifyUserBody = require("../middlewares/verifyUserBody.middlewares.js");
 
 
 router.get("/users", [authMW.verifyToken, authMW.isAdmin], userController.findAll);
 router.get("/users/:userId", [authMW.verifyToken, authMW.isAdmin], userController.findById);
-router.put("/users/:userId", [authMW.verifyToken, authMW.isAdmin], userController.updateUserData);
+router.put("/users/:userId", [authMW.verifyToken, authMW.isAdmin, verifyUserBody.validateUserStatusAndUserType], userController.updateUserData);
 
 module.exports = router;
